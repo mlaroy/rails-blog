0298dc656bd79659c8652ab17a928bce70cf42af
Michael LaRoy <mike.laroy@gmail.com>
2016-08-24 20:49:30 -0700

    Add edit, update, show, and index list

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,9 @@
 class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
   end
@@ -13,6 +18,21 @@ class ArticlesController < ApplicationController
       #flash[:notice] = "Fields are empty"
       render 'new'
     end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      #flash[:notice] = "Fields are empty"
+      render 'edit'
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def show
